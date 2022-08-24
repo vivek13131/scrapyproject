ab7c2fce6b412074ded3fcc3fce5e24c6b5c9ad3
VIVEK <vivekravi682@gmail.com>
2022-08-24 19:42:29 +0530

    edit the bayut file to add more data and append the json files

--- bayhut/bayhut/spiders/bayut.py
+++ bayhut/bayhut/spiders/bayut.py
@@ -3,6 +3,7 @@ import scrapy
 class BayutSpider(scrapy.Spider):
     name ='bayut'
     start_urls =['https://www.bayut.com/to-rent/property/dubai/']
+    base_url =['https://www.bayut.com/']
     
     def parse(self, response,):
         for products in response.css('article.ca2f5674'):
@@ -13,12 +14,18 @@ class BayutSpider(scrapy.Spider):
                 'link':products.css('a._287661cb').attrib['href'] ,
                 
             }
+        # next_page_partial_url = response.xpath('//li[@class="b7880daf"]/a/@href').extract_first()
+        # next_page_url =self.base_url + next_page_partial_url
+        # yield scrapy.Request(next_page_url, callback=self.parse)
         
         
         
         next_page =  response.css('a.b7880daf').attrib['href']
         if next_page is not  None:
             yield response.follow(next_page, callback=self.parse)
+        
+    
+    
         
             
     
