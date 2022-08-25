05d5a64715ec0f85fa1c5755d571afdcf7269b5e
VIVEK <vivekravi682@gmail.com>
2022-08-26 01:28:24 +0530

    update on the json file to 2400 items by using the pagecount keyword

--- bayhut/bayhut/spiders/bayut.py
+++ bayhut/bayhut/spiders/bayut.py
@@ -5,15 +5,15 @@ class BayutSpider(scrapy.Spider):
     name ='bayut'
     start_urls =['https://www.bayut.com/to-rent/property/dubai/']
     base_url =['https://www.bayut.com/to-rent/property/dubai/']
-    custom_settings = {'CLOSESPIDER_PAGECOUNT': 20}
+    custom_settings = {'CLOSESPIDER_PAGECOUNT': 100}
     
     def parse(self, response,):
         for products in response.css('article.ca2f5674'):
             yield{
                 'location':products.css('div._7afabd84::text').get(),
-                # 'price':products.css('span.f343d9ce::text')[1500].get(),
-                # 'type':products.css('div._9a4e3964::text')[1500].get() ,
-                # 'link':products.css('a._287661cb')[1500].attrib['href'] ,
+                'price':products.css('span.f343d9ce::text').get(),
+                'type':products.css('div._9a4e3964::text').get() ,
+                'link':products.css('a._287661cb').attrib['href'] ,
                 
             }
    
