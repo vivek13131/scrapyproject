ce16b56a50454b994ad83ada56a1d3e7cd6bb89d
VIVEK <vivekravi682@gmail.com>
2022-08-26 01:23:29 +0530

    add a new  setting to stop the page count and set a limits the page counting

--- bayhut/bayhut/spiders/bayut.py
+++ bayhut/bayhut/spiders/bayut.py
@@ -1,29 +1,31 @@
+from email.policy import default
 from gc import callbacks
 import scrapy
 class BayutSpider(scrapy.Spider):
     name ='bayut'
-    start_urls =['https://www.bayut.com/property/details-5118159.html']
+    start_urls =['https://www.bayut.com/to-rent/property/dubai/']
     base_url =['https://www.bayut.com/to-rent/property/dubai/']
+    custom_settings = {'CLOSESPIDER_PAGECOUNT': 20}
     
     def parse(self, response,):
         for products in response.css('article.ca2f5674'):
             yield{
                 'location':products.css('div._7afabd84::text').get(),
-                'price':products.css('span.f343d9ce::text').get(),
-                'type':products.css('div._9a4e3964::text').get() ,
-                'link':products.css('a._287661cb').attrib['href'] ,
+                # 'price':products.css('span.f343d9ce::text')[1500].get(),
+                # 'type':products.css('div._9a4e3964::text')[1500].get() ,
+                # 'link':products.css('a._287661cb')[1500].attrib['href'] ,
                 
             }
    
         for next_page in response.css('a.b7880daf'):
             yield response.follow(next_page, self.parse)
     
-    def parse(self, response,):
-          yield{
+    # def parse(self, response,):
+    #       yield{
               
-              'productit':response.xpath("//span[@class='_327a3afc']/text()")[4].extract(),
+    #           'productit':response.xpath("//span[@class='_327a3afc']/text()")[4].extract(),
                 
-          }
+    #       }
           
              
     
